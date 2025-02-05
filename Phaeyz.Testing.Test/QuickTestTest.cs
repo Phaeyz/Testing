09107f0b981f95effac8825c94012b47f128002e
Phaeyz <phaeyz@pm.me>
2025-02-04 20:01:19 -0800

    Add initial source files (#1)

---
+++ Phaeyz.Testing.Test/QuickTestTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Phaeyz.Testing.Test;
+
+internal class QuickTestTest
+{
+    [Test]
+    public async Task ReturnString_BasicString_ReturnsBasicString()
+    {
+        QuickTest quickTest = new();
+        await Assert.That(quickTest.ReturnString("test")).IsEqualTo("test");
+    }
+}
---
+++ Phaeyz.Testing/QuickTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Phaeyz.Testing;
+
+public class QuickTest
+{
+    public string ReturnString(string input) => input;
+}
